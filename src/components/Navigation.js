a5021d3949543b8de9849a11d5e3ddac08e8ba27
Monica Chavarria <monicachavarria.c@gmail.com>
2023-11-24 17:25:39 +0100

    AddButtoninNavbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,19 +1,17 @@
 import logo from "../assets/images/logo.png";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 export const Navigation = () => {
+  /* con location puedo saber donde estoy en todo momento */
+  const location = useLocation();
+
+  /* en la variable pathname, sé en todo momento cual es mi ruta */
+  const isActiveRoute = (route) => {
+    return location.pathname === route;
+  };
   return (
     <>
-      <nav
-        className="navbar navbar-expand-lg navbar-light"
-        style={{
-          backgroundColor: "black",
-          position: "fixed",
-          top: 0,
-          width: "100%",
-          zIndex: 1000,
-        }}
-      >
-        <div className="container-fluid">
+      <nav className="navbar fixed-top navbar-expand-sm navbar-dark bg-dark">
+        <div className="container">
           <img
             className="d-inline-block align-center"
             src={logo}
@@ -21,51 +19,66 @@ export const Navigation = () => {
             width="60"
             height="60"
           />
+
+          <button
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            className="navbar-toggler"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
           <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav text-center d-flex ms-auto">
-              {" "}
-              {/* poner ms-auto me alinea el menú a la derecha */}
-              <li className="nav-item">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item" style={{ margin: "10px" }}>
                 <NavLink
                   to="/"
-                  className="nav-link"
-                  activeClassName="active"
-                  style={{ color: "rgb(240, 170, 105)" }}
+                  style={({ isActive }) => ({
+                    color: isActiveRoute("/") ? "white" : "rgb(240, 170, 105)",
+                  })}
                 >
                   INICIO
                 </NavLink>
               </li>
-              <li className="nav-item">
+              <li className="nav-item" style={{ margin: "10px" }}>
                 <NavLink
                   to="/services"
-                  className="nav-link"
-                  activeClassName="active"
-                  style={{ color: "rgb(240, 170, 105)" }}
+                  style={({ isActive }) => ({
+                    color: isActiveRoute("/services")
+                      ? "white"
+                      : "rgb(240, 170, 105)",
+                  })}
                 >
                   SERVICIOS
                 </NavLink>
               </li>
-              <li className="nav-item">
+              <li className="nav-item" style={{ margin: "10px" }}>
                 <NavLink
                   to="/aboutme"
-                  className="nav-link"
-                  activeClassName="active"
-                  style={{ color: "rgb(240, 170, 105)" }}
+                  style={({ isActive }) => ({
+                    color: isActiveRoute("/aboutme")
+                      ? "white"
+                      : "rgb(240, 170, 105)",
+                  })}
                 >
                   SOBRE MÍ
                 </NavLink>
               </li>
-              <li className="nav-item">
-                              <NavLink
+              <li className="nav-item" style={{ margin: "10px" }}>
+                <NavLink
                   to="/contact"
-                  className="nav-link"
-                  activeClassName="active"
-                  style={{ color: "rgb(240, 170, 105)" }}
+                  style={({ isActive }) => ({
+                    color: isActiveRoute("/contact")
+                      ? "white"
+                      : "rgb(240, 170, 105)",
+                  })}
                 >
-                  CONTACTO 
+                  CONTACTO
                 </NavLink>
-
-
               </li>
             </ul>
           </div>
